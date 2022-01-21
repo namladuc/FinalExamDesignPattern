6643ce2d6ea471634722e7bb009a1406bb2316a1
master_chef_2811 <assassinnyt209@gmail.com>
2022-01-21 22:20:20 +0700

    Add singleton pattern initialized

---
+++ singleton/singletonsolution/BillPughSingleton.java
@@ -0,0 +1,15 @@
+package singleton.singletonsolution;
+
+public class BillPughSingleton {
+    private BillPughSingleton() {
+
+    }
+
+    public static BillPughSingleton getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+
+    private static class SingletonHelper {
+        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
+}
